2bcf085b08ae8c4ba30c817b6ec0340a313df06c
Shekhar Sunuwar <129270764+sunuwarshekhar@users.noreply.github.com>
2026-08-15 23:04:00 +0600

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        let operator = ['+', '-', '*', '/']
+        let stack = []
+        for(let i =0; i < tokens.length; i++){
+            if(isNaN(tokens[i]) && operator.includes(tokens[i])){
+                let a = stack.pop()
+                let b = stack.pop()
+                switch(tokens[i]){               
+                    case '+':
+                        stack.push(a + b)
+                        break;
+                    case '-':
+                        stack.push(b - a)
+                        break;
+                    case '*':
+                        stack.push(a * b)
+                        break;
+                    case '/':
+                        stack.push(Math.trunc(b/a))
+                        break;
+                    default:
+                        return
+                }
+            }else{
+                let num = Number(tokens[i])
+                stack.push(num)
+            }
+        }
+        return stack[0]
+    }
+}
