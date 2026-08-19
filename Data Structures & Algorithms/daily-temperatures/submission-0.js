fd2a57cb9e60b0d4bb417986b1f04e2c37e7b2f1
Shekhar Sunuwar <129270764+sunuwarshekhar@users.noreply.github.com>
2026-08-19 22:32:37 +0600

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let stack = []
+        let res = new Array(temperatures.length).fill(0)
+        for(let i = 0; i < temperatures.length; i++){
+            
+            while(stack.length > 0 && temperatures[stack[stack.length -1]] < temperatures[i]){
+                const pastIdx = stack[stack.length - 1]
+                res[pastIdx] = i - pastIdx 
+                stack.pop()
+            }
+            stack.push(i)
+        }
+        return res
+    }
+}
